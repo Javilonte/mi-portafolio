4e3ad18926197eebc3531a203e3974f54ede8a0d
Javilonte <javgofi@gmail.com>
2024-10-27 01:48:23 -0600

    first commit

---
+++ script.js
@@ -0,0 +1,5 @@
+// Modo oscuro
+const toggleModoOscuro = document.getElementById('modo-oscuro');
+toggleModoOscuro.addEventListener('click', () => {
+    document.body.classList.toggle('oscuro');
+});
